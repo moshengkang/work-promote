09d0058db02b1bd9ae90a7bf82d7d12506cd82ac
xiaokang_glo <xiaokang_glo@qq.com>
2019-12-22 20:34:34 +0800

    可重入所

--- juc-jvm/src/main/java/com/keepstudy/lock/ReentrantLockDemo.java
+++ juc-jvm/src/main/java/com/keepstudy/lock/ReentrantLockDemo.java
@@ -8,7 +8,10 @@ import java.util.concurrent.locks.ReentrantLock;
  * @Author: moshengkang
  * @e-mial: 1634414600@qq.com
  * @Version: 1.0
- * @Description: 可重入锁，也即是说，线程可以进入任何一个它已经拥有的锁所同步着的代码块；最大作用是避免死锁
+ * @Description: 可重入锁（也叫递归锁），也即是说，
+ * 指的是同一先生外层函数获得锁后,内层敌对函数任然能获取该锁的代码
+ * 在同一线程外外层方法获取锁的时候,在进入内层方法会自动获取锁
+ * 线程可以进入任何一个它已经拥有的锁所同步着的代码块；最大作用是避免死锁
  * 默认是非公平锁，非公平锁比公平锁吞吐量大
  */
 class Phone implements Runnable {
