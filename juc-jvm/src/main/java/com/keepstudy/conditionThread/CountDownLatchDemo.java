5c38e857f7f58b38bcfa6d9ad9bac161c8cb302f
xiaokang_glo <xiaokang_glo@qq.com>
2019-12-23 21:47:05 +0800

    CountDownLatch解释说明

--- juc-jvm/src/main/java/com/keepstudy/conditionThread/CountDownLatchDemo.java
+++ juc-jvm/src/main/java/com/keepstudy/conditionThread/CountDownLatchDemo.java
@@ -7,6 +7,9 @@ import java.util.concurrent.CountDownLatch;
  * @e-mail: 1634414600@qq.com
  * @Date: 2019/12/23 18:40
  * @Description: CountDownLatchDemo
+ * CountDownLatch主要有两个方法,当一个或多个线程调用await方法时,调用线程会被阻塞.
+ * 其他线程调用countDown方法计数器减1(调用countDown方法时线程不会阻塞),
+ * 当计数器的值变为0,因调用await方法被阻塞的线程会被唤醒,继续执行.
  */
 public class CountDownLatchDemo {
     public static void main(String[] args) {
