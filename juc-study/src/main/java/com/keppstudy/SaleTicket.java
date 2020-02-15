72f2a14a66c03562bc71124772905558a3dd505a
xiaokang_glo <xiaokang_glo@qq.com>
2020-02-15 16:44:46 +0800

    juc学习

---
+++ juc-study/src/main/java/com/keppstudy/SaleTicket.java
@@ -0,0 +1,60 @@
+package com.keppstudy;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @Author: moshengkang
+ * @e-mial: 1634414600@qq.com
+ * @Version: 1.0
+ * @Description: lock
+ */
+class Ticket {
+//    private volatile int number = 30;
+    private volatile int number = 30;
+    Lock lock = new ReentrantLock();
+
+    public void sale (){
+        lock.lock();
+        try {
+            if (number > 0) {
+                System.out.println(Thread.currentThread().getName()+"\t 卖出第："+(number--)+"\t 还剩"+number);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+
+public class SaleTicket {
+    public static void main(String[] args) {
+        Ticket ticket = new Ticket();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 40; i++) {
+                    ticket.sale();
+                }
+            }
+        },"A").start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 40; i++) {
+                    ticket.sale();
+                }
+            }
+        },"B").start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 40; i++) {
+                    ticket.sale();
+                }
+            }
+        },"C").start();
+    }
+}
